bacc14b02b886f4e28c9e53d5615033501bd6f00
jawad-star <jawwadsaleem913@gmail.com>
2025-07-25 09:34:53 +0500

    Initial project setup and configuration - Set up React Native Expo project with TypeScript - Configure ESLint and project structure - Add package.json with required dependencies - Set up basic app configuration

---
+++ Jorvea/app.config.js
@@ -0,0 +1,54 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "Jorvea",
+    slug: "Jorvea",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "jorvea",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      googleServicesFile: "./GoogleService-Info.plist",
+      supportsTablet: true,
+      bundleIdentifier: "com.jawadxdev.Jorvea"
+    },
+    android: {
+      googleServicesFile: "./google-services.json",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.jawadxdev.Jorvea"
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "@react-native-google-signin/google-signin",
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      webClientId: process.env.WEB_CLIENT_ID,
+      androidClientId: process.env.ANDROID_CLIENT_ID,
+      iosClientId: process.env.IOS_CLIENT_ID,
+    }
+  }
+};
